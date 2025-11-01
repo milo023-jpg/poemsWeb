4bf08834990093d85fe080704df66b3f3841f951
Camilo <miloar023@gmail.com>
2025-10-31 23:25:15 -0500

    Arreglo JSON salto de linea

--- js/pruebas.js
+++ js/pruebas.js
@@ -4,7 +4,8 @@ import { obtenerPoemaDelDia } from './poemsManager.js';
 async function init() {
   try {
     // 1️⃣ Cargar poemas y progreso desde Firebase
-    const res = await fetch('data/poemas.json');
+    //const res = await fetch('data/poemas.json');
+    const res = await fetch('https://raw.githubusercontent.com/milo023-jpg/poemsWeb/refs/heads/main/data/poemas.json');
     const poemas = await res.json();
     const progreso = await obtenerProgreso();
 
